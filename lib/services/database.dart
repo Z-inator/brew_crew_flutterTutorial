7041cc0df2f3f6ef3f491f1e3ee5a6133220fef1
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-02-02 16:39:17 -0600

    Sections 13-20

---
+++ lib/services/database.dart
@@ -0,0 +1,36 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+
+  // collection reference
+  final CollectionReference brewCollection =
+      Firestore.instance.collection('brews');
+
+  Future<void> updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+  // Brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+        name: doc.data['name'] ?? '',
+        sugars: doc.data['sugars'] ?? '0',
+        strength: doc.data['strength'] ?? 0
+      );
+    }).toList();
+  }
+
+  // Get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+}
